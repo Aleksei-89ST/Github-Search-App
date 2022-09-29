2f8428430b8700088f258b5b3ce11867c1f838f8
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-09-29 14:40:58 +0400

    logic add and remove

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-
-
 import { githubApi } from "./github/github.api";
+import { githubReducer } from "./github/github.slice";
 
 export const store = configureStore({
     reducer: {
-        [githubApi.reducerPath]: githubApi.reducer
+        [githubApi.reducerPath]: githubApi.reducer,
+        github: githubReducer
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(githubApi.middleware)
 })
+
+// для корректной настройки типизации-кастомные типы для понимания того скакими данными рабатаю в store
+export type RootState = ReturnType<typeof store.getState>
